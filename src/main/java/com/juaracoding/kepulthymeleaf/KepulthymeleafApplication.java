295a1c59fe709823acbf8733727ebb2988f9fd54
fazair1 <fauzan.irfanto1@gmail.com>
2025-04-11 21:06:55 +0700

    Initial commit

---
+++ src/main/java/com/juaracoding/kepulthymeleaf/KepulthymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.juaracoding.kepulthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KepulthymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KepulthymeleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juaracoding/kepulthymeleaf/KepulthymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juaracoding.kepulthymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KepulthymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
